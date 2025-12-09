77f296e77021d2f4b2d248164b51b5155249afea
2397931 <143234904+2397931@users.noreply.github.com>
2025-12-08 22:01:37 -0500

    renommer img / map

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,10 @@
-import { animate } from 'animejs';
+import maplibregl from "maplibre-gl";
 
-animate('.icones', {
-    rotate: 360
+const carte = new maplibregl.Map({
+  container: "carte",
+  style: "https://tiles.openfreemap.org/styles/dark",
+  center: [139.1104464, 35.5074466],
+  zoom: 6
 });
+
+
